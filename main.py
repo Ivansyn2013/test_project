ebe29c8893831cd60e921725150e2844ff6ff4af
ivan <ivansyn2013@yandex.ru>
2021-11-08 11:26:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print ('Hellow world')
+'''
+my_list_1 = [2, 5, 8, 2, 12, 12,4]
+print(len(my_list_1))
+my_list_2 = [2, 7, 12,3]
+del_list = []
+i = 0
+
+for i  in range(len(my_list_2)):
+
+    if my_list_2[i] in my_list_1:
+        my_list_1.remove(my_list_2[i])
+'''
+
+date = '02.03.2012'
+days = ['первое','второе', 'третье', 'четвертое', 'пятое', 'шестое']
+months = ['январь','ферраль','март','апрель']
+
+datasplit = (date.split('.'))
+if datasplit[]
+
+print(datasplit)
+'''     
+my_list_1 = [2, 5, 8, 2, 12, 12,4]
+my_list_2 = [2, 7, 12,3]
+
+my_list_1 = set(my_list_1)
+my_list_2 = set(my_list_2)
+
+result = my_list_1 - my_list_2
+
+print(result)
+'''
